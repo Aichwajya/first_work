97277e7bcaedb7b5e871d86b1f1991b40098fb90
Aichwajya <43476482+Aichwajya@users.noreply.github.com>
2018-09-21 21:09:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,227 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Blood Bank informtion System(BBIS)</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery-1.11.0.min.js"></script>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Donate Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--Google Fonts-->
+<link href='//fonts.googleapis.com/css?family=Karla:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Archivo+Narrow:400,700' rel='stylesheet' type='text/css'>
+
+		<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+		<script src="js/wow.min.js"></script>
+		<script>
+		 new WOW().init();
+		</script>
+<!-- animated-css -->
+<script src="js/responsiveslides.min.js"></script>
+ <script>
+    // You can also use "$(window).load(function() {"
+    $(function () {
+      // Slideshow 1
+      $("#slider1").responsiveSlides({
+         auto: true,
+		 nav: true,
+		 speed: 500,
+		 namespace: "callbacks",
+      });
+    });
+  </script>
+
+</head>
+<body>
+<!--banner start here-->
+<div class="ban-mother-grid">
+<div class="slider">
+		    <ul class="rslides" id="slider1">
+		      <li>
+		      	<div class="banner">
+					
+				</div>
+		      </li>
+		      <li>
+		      	<div class="banner2">
+					
+				</div>
+		      </li>
+		      <li>
+		      	<div class="banner3">
+					
+				</div>
+		      </li>
+		    </ul>
+ </div>	
+<div class="header">
+	<div class="container">
+			   <div class="logo wow bounceInLeft" data-wow-delay="0.3s">
+			     <h1><a href="index.php">BBIS</a></h1>
+				 <h4>(Blood Bank Info. System)</h4>
+			   </div>
+			   <div class="header-icons">
+			   	 <ul>
+			   	 	<li><a href="https://www.facebook.com/Blood-Bank-Information-System-1568992976507861" class="fb" target="_blank"> </a></li>
+			   	 	<li><a href="#" class="twit"> </a></li>
+			   	 	<li><a href="#" class="gmail"> </a></li>
+			   	 	<li><a href="#" class="dri"> </a></li>
+			   	 </ul>
+			   </div>
+			    <div class="clearfix"> </div>
+    </div>	    
+</div>
+ <!--navgation start here-->
+<div class="top-nav">
+    	 <span class="menu"> <img src="images/icon.png" alt=""></span>	
+		<?php require('header.php');?>
+	<!-- /script-for-menu -->
+</div>	
+</div>
+<!--navgation end here-->
+
+<!--vedio block start here-->
+<div class="vedio">
+	<div class="container">
+		<div class="vedio-main">
+			<div class="col-md-6 vedio-left wow fadeInLeft" data-wow-delay="0.3s">
+				
+				<p>Blood is universally recognized as the most precious element that sustains life. It saves innumerable lives across the world in a variety of conditions. The need for blood is great - on any given day, approximately 39,000 units of Red Blood Cells are needed.
+							More than 29 million units of blood components are transfused every year.
+							Donate Blood Despite the increase in the number of donors, blood remains in short supply during emergencies, mainly attributed to the lack of information and accessibility.
+							We positively believe this tool can overcome most of these challenges by effectively connecting the blood donors with the blood recipients.</p>
+			</div>
+			
+		  <div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+<!--vedio block end here-->
+<!--charity strip start here-->
+<div class="charity">
+	<div class="container">
+		<div class="charity-main wow fadeInRight" data-wow-delay="0.3s">
+			<h3>"No one has ever become Poor by Donating Blood" </h3>
+
+		</div>
+	</div>
+</div>
+<!--charity strip end here-->
+<!--donate start here-->
+<div class="donayte">
+	<div class="container">
+		<div class="donate-main">
+			<div class="col-md-3 donate-grid wow fadeInLeft" data-wow-delay="0.3s">
+				<h3>Donate</h3>
+				<h4>Our Gallery</h4>
+				<p>Images of some Blood Donation camp held in different places...</p>
+			</div>
+			<div class="col-md-3 donate-grid wow fadeInLeft" data-wow-delay="0.3s">
+				<div class="fall-down-effect top">
+							<div class="img-box"><img src="images/d1.jpg" alt="" height=200px width=300px></div>
+							<div class="text-box">
+								<div class="text-content">
+									<h3>Donate</h3>																																		
+								</div>
+							</div>
+						</div>
+				<h5>At Lions club, Jorhat</h5>	
+				<p>A camp held on 25th april 2017...</p>
+			</div>
+			<div class="col-md-3 donate-grid wow fadeInRight" data-wow-delay="0.3s">
+				<div class="fall-down-effect top">
+							<div class="img-box"><img src="images/d2.jpg" alt="" height=200px width=300px ></div>
+							<div class="text-box">
+								<div class="text-content">
+									<h3>Donate</h3>																																		
+								</div>
+							</div>
+						</div>
+				<h5>At Kaziranga University, Jorhat</h5>		
+				<p>A camp held on 2nd May 2017...</p>			
+			</div>
+			<div class="col-md-3 donate-grid wow fadeInRight" data-wow-delay="0.3s">
+				<div class="fall-down-effect top">
+							<div class="img-box"><img src="images/d3.jpg" alt="" height=200px width=300px></div>
+							<div class="text-box">
+								<div class="text-content">
+									<h3>Donate</h3>																																		
+								</div>
+							</div>
+						</div>
+				<h5>At J.B College, Jorhat</h5>		
+				<p>A camp held on 10th May 2017...</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="footer">
+	<div class="container">
+		<div class="footer-main">
+			<div class="col-md-4 ftr-grid">
+				<h3>Navigation</h3>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="donar.php">Info for Donor</a></li>
+					<li><a href="Admin/index.php">Admin LogIn</a></li>
+					<li><a href="login.php">Donor Login</a></li>
+				 
+				</ul>
+			</div>
+			<div class="col-md-4 ftr-grid">
+				
+			</div>
+		
+			<div class="col-md-4 ftr-grid">
+				<h3>Keep In Touch</h3>
+				<div class="ftr-icons">
+			    	  	 <div class="ftr-iblock">
+			        	   <span class="glyphicon glyphicon-map-marker">  </span>
+			        	 </div>
+			        	 <div class="ftr-text">
+			        	 	<p>Kaziranga University, Koraikhuwa, JORHAT-785006, ASSAM</p>
+			        	 </div>
+			        	 <div class="clearfix"> </div>
+			   </div>
+			   <div class="ftr-icons">
+			    	  	 <div class="ftr-iblock">
+			        	   <span class="glyphicon glyphicon-earphone">  </span>
+			        	 </div>
+			        	 <div class="ftr-text">
+			        	 	<p>+91 1800-265-2020</p>
+			        	 </div>
+			        	 <div class="clearfix"> </div>
+			   </div>
+			   <div class="ftr-icons">
+			    	  	 <div class="ftr-iblock">
+			        	   <span class="glyphicon glyphicon-envelope">  </span>
+			        	 </div>
+			        	 <div class="ftr-text">
+			        	 	<p>BBIS.ku17@gmail.com</p>
+			        	 </div>
+			        	 <div class="clearfix"> </div>
+			   </div>
+			</div>
+			<div class="clearfix"> </div>		
+		</div>
+	</div>
+</div>
+<!--footer end here-->
+<!--copy rights start here-->
+<div class="copy-right">
+	<div class="container">
+		 <div class="copy-rights-main">
+    	    <p>© 2017 Donate. All Rights Reserved | Design by Aichwajya, Nawab, Nishanta </p>
+    	 </div>
+    </div>
+</div>
+<!--copy right end here-->
+</body>
+</html>		 
